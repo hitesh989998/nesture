b5250ccceb36fbcb9d5f0bdb7c37bc5ce614f451
hitesh989998 <hitesh989998@gmail.com>
2025-01-03 04:08:47 +0530

    Authorization completed + global authorisation state + frontend default dp setup + nav menu changes

---
+++ backend/controllers/userHandler.js
@@ -0,0 +1,6 @@
+const dashboardUser = (req, res) => {
+ console.log(req.user);
+  res.status(200).json({ message: "Dashboard" });   
+}
+
+module.exports = dashboardUser
---
+++ backend/middlewares/AuthorizationMW.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  
+  try {
+    const token = req.cookies.token;
+    const decodedUser = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    req.user = decodedUser;
+    console.log(decodedUser,'this is the decoded user');
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ message: 'Invalid Token' });
+  }
+};
+
+module.exports = authMiddleware;
